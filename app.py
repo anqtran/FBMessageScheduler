780aba432a668ce7cb99a0625793a7b497d4ad4e
An Tran <anqtran@gatech.edu>
2018-01-27 13:01:55 -0500

    begin hack

--- app.py
+++ app.py
@@ -8,51 +8,32 @@ from fbchat.models import *
 app = Flask(__name__)
  
 @app.route('/')
-def home(facebook_client):
-    if facebook_client is None:
-        return render_template('login.html')
-
-    friends = facebook_client.fetchAllUsers()
-    print(facebook_client)
-    for friend in friends:
-        print(friend.name)
-
+def home():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        return render_template('friends_list.html', friends=friends)
+        return render_template('friends_list.html')
  
 @app.route('/login', methods=['POST'])
 def do_admin_login():
-
     username = request.form['username']
     password = request.form['password']
 
     try:
         client = Client(username, password, max_tries=1)
         session['logged_in'] = True
-        # facebook_client = client
+        friends = client.fetchAllUsers()
+        return render_template('friends_list.html', friends=friends)
     except FBchatException as exception:
         print(exception)
         flash('wrong password')
 
-    return home(client)
-
-    # if request.form['password'] == 'password' and request.form['username'] == 'admin':
-    #     session['logged_in'] = True
-    # else:
-    #     flash('wrong password!')
-    # return home()
-    # if request.form['password'] == 'password' and request.form['username'] == 'admin':
-    #     session['logged_in'] = True
-    # else:
-    #     flash('wrong password!')
-    # return home()
+    return home()
 
 @app.route("/logout")
 def logout():
     session['logged_in'] = False
-    return home(None)
+    return home()
 
  
 if __name__ == "__main__":
