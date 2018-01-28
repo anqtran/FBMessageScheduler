34a7ed2bba417663f6773d614bbd6c70edf5ddae
An Tran <anqtran@gatech.edu>
2018-01-27 21:04:51 -0500

    job scheduler still not sending message

--- app.py
+++ app.py
@@ -19,16 +19,18 @@ scheduler = BackgroundScheduler()
 
 client = None
 friends = None
-
+queue = None
 
 # Background Job
-# def check_for_jobs():
-#     # if client:
-#         # client.send(Message(text='Message using cookies'), thread_id='100000046489168', thread_type=ThreadType.USER)
-#     print('I am working...')
+def check_for_jobs():
+    # if client:
+        # client.send(Message(text='Message using cookies'), thread_id='100000046489168', thread_type=ThreadType.USER)
+    if queue:
+        print(queue)
+    print('I am working...')
 
-# scheduler.add_job(check_for_jobs, 'interval', seconds=5)
-# scheduler.start()
+scheduler.add_job(check_for_jobs, 'interval', seconds=5)
+scheduler.start()
 
 # User input form
 class UserInput(FlaskForm):
@@ -38,15 +40,38 @@ class UserInput(FlaskForm):
     timestamp = DateTimeField(label='Date', default=datetime.today, format='%Y-%m-%d %H:%M:%S')
     sendbtn = SubmitField('Send')
 
+# Queue for jobs
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def add_job(self, username, pw, recipient_uid, message, timestamp):
+        self.queue.append({
+            'username': username,
+            'pw': pw,
+            'recipient_uid': recipient_uid,
+            'message': message, 
+            'timestamp': timestamp
+            })
+
+    def __str__(self):
+        return str(self.queue)
+
+
 @app.route("/send/", methods=['POST'])
 def send():
+    global queue
+    global client
     data_form = {}
     for key, value in request.form.items():
         data_form[key] = value
 
-    friend = data_form['friends']
+    friend_id = data_form['friends']
     message = data_form['message']
+    timestamp = data_form['timestamp']
     client.send(Message(text=message), thread_id='100000007282966', thread_type=ThreadType.USER)
+    queue.add_job(client.email, client.password, friend_id, message, timestamp)
+    print(client.email, client.password, friend_id, message, timestamp)
     return redirect('/')
 
 @app.route('/')
@@ -80,6 +105,7 @@ def logout():
     return home()
 
 if __name__ == "__main__":
+    queue = Queue()
     app.secret_key = os.urandom(12)
     app.run(debug=True,host='0.0.0.0', port=4000)
 
